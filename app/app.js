9ddc20f514e34a90b05ea45495f55d1d640b5b6a
Matthew Huk <matthew.huk@bglgroup.co.uk>
2022-01-24 16:10:50 +0000

    Update the default meerkat list

--- app/app.js
+++ app/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+const meerkats=[
+    {name: "Aleksandr", age: 30},
+    {name: "Sergei", age:77},
+    {name: "Maiya", age: 25},
+    {name: "Oleg", age: 5}
+];
+
 app.get('/', function (req, res) {
     res.send('Hello Shraddha')
 });
-
 
 app.listen(3000, function () {
     console.log('App listening on port 3000!')
